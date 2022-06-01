7901c780f9c02b625630dfd56a61b6e4a52e5250
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2022-06-01 23:59:17 +0530

    Adding more routes

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask 
+from flask import Flask #importing Flask class from flask module
 
 app = Flask(__name__) 
 
 @app.route('/') 
-
+@app.route('/home')
 def hello_world(): 
-    return "Hello world"
+    return "<h1><b>Hello world</b></h1>"+__name__
+
+@app.route('/about') 
+def about(): 
+    return "<h1><b>Hello world</b></h1>"+__name__
 
 if __name__=="__main__": 
-    app.run(debug=True) 
+    app.run(debug=True)
