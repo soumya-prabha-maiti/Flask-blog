2c2c91304481dea428368ccf0db7c78fc99ee441
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2022-06-02 00:23:49 +0530

    Added templates

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask #importing Flask class from flask module
+from flask import Flask,render_template
 
 app = Flask(__name__) 
 
 @app.route('/') 
 @app.route('/home')
-def hello_world(): 
-    return "<h1><b>Hello world</b></h1>"+__name__
+def home(): 
+    return render_template('home.html')
 
 @app.route('/about') 
 def about(): 
-    return "<h1><b>Hello world</b></h1>"+__name__
+    return "<h1><b>About page</b></h1>"
 
 if __name__=="__main__": 
     app.run(debug=True)
