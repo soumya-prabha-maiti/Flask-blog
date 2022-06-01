27552cf1298489c9fe4dc1c1fdb779d2e5fbe8a9
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2022-06-02 00:39:29 +0530

    Templates and using jinja2 & if else for loops

--- app.py
+++ app.py
@@ -2,14 +2,28 @@ from flask import Flask,render_template
 
 app = Flask(__name__) 
 
+blogPosts=[
+    {
+        'author':'Me',
+        'title':'Post 1',
+        'content':'Hi !',
+    },
+    {
+        'author':'Friend',
+        'title':'New post',
+        'content':'Hello',
+    },
+]
+
 @app.route('/') 
 @app.route('/home')
 def home(): 
-    return render_template('home.html')
+    return render_template('home.html',posts=blogPosts)
 
 @app.route('/about') 
 def about(): 
-    return "<h1><b>About page</b></h1>"
+    return render_template('about.html',newTitle='About')
+
 
 if __name__=="__main__": 
     app.run(debug=True)
