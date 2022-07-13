a2d955c473f017096de3aa9adb8af5de70e36096
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2022-07-13 13:18:42 +0530

    fix: unique constraint failure of email & username
    Add custom validators to catch already used username & email

--- flaskblog/appForms.py
+++ flaskblog/appForms.py
@@ -1,7 +1,8 @@
 import email
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,SubmitField,BooleanField
-from wtforms.validators import DataRequired,length,Email,EqualTo
+from wtforms.validators import DataRequired,length,Email,EqualTo,ValidationError
+from flaskblog.models import User
 
 class SignUpForm(FlaskForm):
     username = StringField('Username',validators=[DataRequired(),length(min=2,max=50)])
@@ -9,6 +10,21 @@ class SignUpForm(FlaskForm):
     password=PasswordField('Password',validators=[DataRequired()])
     confirmPassword=PasswordField('Confirm Password',validators=[DataRequired(),EqualTo('password',message="Passwords do not match")])
     submit=SubmitField(label="Sign Up")
+
+    # Template of custom validator: 
+    # def validate_field(self,field):
+    #     if condition:
+    #         raise ValidationError('Validation message')
+
+    def validate_username(self,username):
+        user=User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is already taken. Please choose a different one')
+
+    def validate_email(self,email):
+        user=User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email is already taken. Please choose a different one')
 
 class LoginForm(FlaskForm):
     email=StringField('Email',validators=[DataRequired(),Email()])#Login with email
