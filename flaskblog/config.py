5eaf472dfd02101a6ecc4aafd69a3e8fcad5797f
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2022-08-05 13:32:56 +0530

    refactor: Load configuration form json

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,11 +1,15 @@
 import os
+import json
+
+with open("/etc/flask_blog_config.json",) as config_file:
+    config=json.load(config_file)
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")  # //// used for absolute path; ///used for relative path with respect to the project folder
+    SECRET_KEY = config.get("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = config.get("SQLALCHEMY_DATABASE_URI")  # //// used for absolute path; ///used for relative path with respect to the project folder
     MAIL_SERVER = "smtp.gmail.com"
     MAIL_PORT = "587"
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("EMAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+    MAIL_USERNAME = config.get("EMAIL_USERNAME")
+    MAIL_PASSWORD = config.get("EMAIL_PASSWORD")
