1637969a3a16ed87a9edc43fab558f43dfb3df22
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2022-08-05 15:19:35 +0530

    fix: Create the database on run, if not present

--- run.py
+++ run.py
@@ -1,4 +1,7 @@
-from flaskblog import create_app
+from flaskblog import create_app,db
 app=create_app()
+
 if __name__=="__main__": 
-    app.run(debug=True) 
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
