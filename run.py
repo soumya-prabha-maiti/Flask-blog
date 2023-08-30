b3a791bdc982cc0cdb6b1381ed1fd6d91f70bcf7
Soumya Prabha Maiti <soumyaprabhamaiti2001@gmail.com>
2023-08-31 01:56:37 +0530

    Move db creation outside __main__

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
 from flaskblog import create_app,db
 app=create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__=="__main__": 
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
